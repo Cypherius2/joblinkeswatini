c97047e2848d58c1e95b0a463efdccffbc55d87f
Cypherius2 <mrcypher68@gmail.com>
2025-09-02 01:29:12 +0200

    feat: set production API URL

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@
 //const API_URL = 'http://127.0.0.1:3000';
 
 // When you deploy, you will comment out the line above and uncomment the line below.
-const API_URL = 'https://joblink-eswatini-api.onrender.com';
+const API_URL = 'https://joblink-eswatini-live.onrender.com';
